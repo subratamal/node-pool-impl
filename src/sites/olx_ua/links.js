d52cdd1b413b632dd414c2d74f8512ae1de0c146
Subrat <06.subrata@gmail.com>
2018-09-21 11:20:30 +0530

    olx links module added

---
+++ src/sites/olx_ua/links.js
@@ -0,0 +1,57 @@
+const commonHelpers = require('./../../helpers/common')
+const LINKS = require('./constants/links')
+
+module.exports = {
+  links: getLinks(),
+  tryFetchLinks: tryFetchLinks,
+  addCategoryLinkLabel
+}
+
+async function tryFetchLinks(options, { logger }) {
+  const { links, fetchLinks } = options
+
+  if (Array.isArray(links)) {
+    return links
+  }
+
+  if (!lodash.isFunction(fetchLinks)) {
+    return []
+  }
+
+  do {
+    try {
+      return await fetchLinks({
+        logger: logger
+      })
+    } catch (error) {
+      logger.error(error, 'fetch links error -> retry')
+      await Promise.delay(1000)
+    }
+  } while (true)
+}
+
+function getLinks() {
+  const links = []
+
+  LINKS.price_range.map(link => {
+    links.push({ category: link })
+  })
+
+  LINKS.single.map(link => {
+    link = commonHelpers.parseLink(link)
+
+    links.push({ category: link.path, params: link.params, single: true })
+  })
+
+  return links
+}
+
+
+function addCategoryLinkLabel(links) {
+  return links.map(link => {
+    return {
+      categoryLink: commonHelpers.standardName(link.category),
+      ...link
+    }
+  })
+}
